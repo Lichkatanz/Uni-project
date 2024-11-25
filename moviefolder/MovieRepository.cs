a78367625ce8da47d59d1d14081e99ba1125043e
Lichkatanz <stu2113811027@uni-plovdiv.bg>
2024-11-25 23:44:36 +0200

    Add files via upload

---
+++ moviefolder/MovieRepository.cs
@@ -0,0 +1,26 @@
+﻿using MovieStore.DL.Interfaces;
+using MovieStore.DL.StaticDB;
+using MovieStore.Models.DTO;
+
+namespace MovieStore.DL.Repositories
+{
+    internal class MovieRepository : IMovieRepository
+    {
+        public List<Movie> GetAllMovies()
+        {
+            return InMemoryDb.Movies;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            InMemoryDb.Movies.Add(movie);
+        }
+
+        public Movie? GetMovieId(int id)
+        {
+           return InMemoryDb.Movies
+               .FirstOrDefault(m => m.Id == id);
+        }
+
+    }
+}
