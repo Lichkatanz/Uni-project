50dd6af19b6089ee8fb978b9485b89ad301d4e84
Lichkatanz <stu2113811027@uni-plovdiv.bg>
2024-12-04 20:09:35 +0200

    up

---
+++ moviefolder/op/MovieStore.Models/View/MovieView.cs
@@ -0,0 +1,11 @@
+namespace MovieStore.Models.DTO
+{
+    public class MovieView
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+    }
+}
