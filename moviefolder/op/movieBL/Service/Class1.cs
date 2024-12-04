560dc31bdebba42b4720b17c4e0349f6aae6c2b9
Lichkatanz <stu2113811027@uni-plovdiv.bg>
2024-12-04 19:54:01 +0200

    update

---
+++ moviefolder/op/movieBL/Service/Class1.cs
@@ -0,0 +1,17 @@
+using MovieStore.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.BL.Services
+{
+    internal class MovieBLService
+    {
+        private readonly IMovieBLService _movieBLService;
+        private readonly IActorRepository _actorRepository;
+    }
+
+    public IEnumarable<>
+}
